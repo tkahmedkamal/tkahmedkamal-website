b95b7f7fa3005723ee150d246892f624b1597904
tkahmedkamal <tkahmedkamal@hotmail.com>
2020-01-31 22:34:33 +0200

    install the normalize.css, and Created a Navbar and the menu area

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,7 @@
 ==================================================*/
 import "bootstrap/dist/css/bootstrap-grid.min.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
+import "normalize.css/normalize.css";
 import "../sass/styles.scss";
 import WOW from "wow.js/dist/wow.min.js";
 import "wow.js/css/libs/animate.css";
@@ -9,3 +10,25 @@ var wow = new WOW({
 	boxClass: "reveal-animate"
 });
 wow.init();
+
+$(function() {
+	"use strict";
+
+	/* Menu Area - FadeIn And FadeOut
+	===================================================== */
+	$("#menu-icon").on("click", function() {
+		$("#menu-area")
+			.fadeIn(400, function() {
+				$("#navbar-area").addClass("active");
+			})
+			.css("display", "flex");
+	});
+
+	$("#close-primary-menu").on("click", function() {
+		$("#navbar-area").removeClass("active");
+		$("#menu-area")
+			.delay(900)
+			.fadeOut(400);
+	});
+	/* Menu Area End */
+});
